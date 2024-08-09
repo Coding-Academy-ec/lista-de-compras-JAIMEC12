cf5fc110bec24dfef74fa3a98347bba7ba48548a
JAIME_CARRIEL <jaimecarriel302@gmail.com>
2024-08-09 14:05:23 -0500

    RESPUESTA

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,13 @@
-# Ingrese una lista de compras: manzanas, plátanos, leche, pan
-# Los productos en la lista de compras son: ['manzanas', 'plátanos', 'leche', 'pan']
-
-#from lib2to3.pytree import convert
-
-
-compras = input("Ingrese una lista de compras: ")
-productos = compras.split(", ")
-print(f"Los productos en la lista de compras son: {productos}")
-
-# Convertir la lista de compras en una tupla
 def convertir_lista_a_tupla(lista):
     return tuple(lista) # Utiliza una función para convertir 
+
+
+def main():
+    compras = input("Ingrese una lista de compras: ")
+    productos = compras.split(", ")
+    print(f"Los productos en la lista de compras son: {productos}")
+    productos=convertir_lista_a_tupla(productos)
+    print(f"Los productos en la lista de compras son: {productos}")
+
+if __name__ == "__main__":
+    main()
